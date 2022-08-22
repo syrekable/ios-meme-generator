26eb985666a478e9e408356962595fe9ce38e596
Jordan Niedzielski <contact@syrekable.tech>
2022-08-22 15:49:54 +0200

    initial ContentView

--- Random Meme Generator/ContentView.swift
+++ Random Meme Generator/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let imageIDs: [String] = ["Sad-Keanu", "Forever-Alone"]
+    @State private var currentImageID = "Sad-Keanu"
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            List {
+                Picker("Meme base:", selection: $currentImageID) {
+                    ForEach(imageIDs, id: \.self) { id in
+                        Text(id)
+                    }
+                }
+            }
+            AsyncImage(url: URL(string: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.imgur.com%2FIFeHHse.jpg&f=1&nofb=1"), scale: 1) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                } else if phase.error != nil {
+                    Color.red // Indicates an error.
+                } else {
+                    Color.orange // Acts as a placeholder.
+                }
+            }
+            Button("Generate meme") {
+                print("Generating...")
+            }
             .padding()
+            .foregroundColor(.orange)
+            .border(.orange)
+        }
+        .padding()
     }
 }
 
