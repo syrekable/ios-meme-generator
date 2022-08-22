d27497ec5a76a75e3c31e485113a5ee97cf578ae
Jordan Niedzielski <contact@syrekable.tech>
2022-08-22 15:06:12 +0200

    Initial Commit

---
+++ Random Meme Generator/Random_Meme_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_Meme_GeneratorApp.swift
+//  Random Meme Generator
+//
+//  Created by Jordan Niedzielski on 22/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Random_Meme_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
